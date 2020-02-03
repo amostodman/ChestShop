2c8a74339f1b585f629f71b4bb4061ca30bf40c3
amos <amostodman@gmail.com>
2020-02-03 03:05:54 -0500

    crash fix?

--- src/ChestShop/EventListener.php
+++ src/ChestShop/EventListener.php
@@ -77,7 +77,7 @@ class EventListener implements Listener
 				$chest->getInventory()->removeItem($item);
 				$player->getInventory()->addItem($item);
 				$sellerMoney = EconomyAPI::getInstance()->myMoney($shopInfo['shopOwner']);
-				if(EconomyAPI::getInstance()->reduceMoney($player->getName(), $shopInfo['price'], false, "ChestShop") === EconomyAPI::RET_SUCCESS and EconomyAPI::getInstance()->addMoney($shopInfo['shopOwner'], $shopInfo['price'], false, "ChestShop") === EconomyAPI::RET_SUCCESS) {
+				if(EconomyAPI::getInstance()->reduceMoney($player->getName(), $shopInfo['price'], null, "ChestShop") === EconomyAPI::RET_SUCCESS and EconomyAPI::getInstance()->addMoney($shopInfo['shopOwner'], $shopInfo['price'], false, "ChestShop") === EconomyAPI::RET_SUCCESS) {
 					$player->sendMessage("Completed transaction");
 					if (($p = $this->plugin->getServer()->getPlayer($shopInfo['shopOwner'])) !== null) {
 						$p->sendMessage("{$player->getName()} purchased ".ItemFactory::get($pID, $pMeta)->getName()." for ".EconomyAPI::getInstance()->getMonetaryUnit().$shopInfo['price']);
